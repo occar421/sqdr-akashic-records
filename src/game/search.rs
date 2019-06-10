18788c913fdfd0a3e8a8a36a74378859e7181b60
MasuqaT <occar@hotmail.co.jp>
2019-06-10 23:24:07 +0900

    make it run

--- src/game/search.rs
+++ src/game/search.rs
@@ -35,45 +35,45 @@ impl Searcher {
     }
 
     fn search_recur<B>(self: &Self, current_board: &B, piece_index: usize) -> Option<Code> where B: Board {
-        println!("{:?}", current_board);
-
         let next = current_board.move_at(piece_index);
 
         let previous_code = current_board.encode();
         return if let Some(board) = next {
             let code = board.encode();
-            if let Some(relation) = self.map.borrow_mut().get_mut(&code) {
-                relation.previous_boards.push(previous_code);
+            {
+                let mut map = self.map.borrow_mut();
 
-                if relation.game_result != GameResult::Unknown {
-                    return Option::Some(code);
-                }
+                if let Some(relation) = map.get_mut(&code) {
+                    relation.previous_boards.push(previous_code);
 
-                for i in 0..B::get_board_size() {
-                    let child_code = self.search_recur(&board, i);
-                    if let Some(child_code) = child_code {
-                        relation.next_boards.try_borrow_mut().unwrap().push(child_code);
+                    if relation.game_result != GameResult::Unknown {
+                        return Option::Some(code);
                     }
-                }
-            } else {
-                let result = board.get_result();
+                } else {
+                    let result = board.get_result();
 
-                let next_boards = Rc::new(RefCell::new(Vec::new()));
+                    let next_boards = Rc::new(RefCell::new(Vec::new()));
 
-                self.map.borrow_mut().insert(board.encode(), BoardRelation { game_result: result, previous_boards: vec![previous_code], next_boards: next_boards.clone() });
+                    map.insert(board.encode(), BoardRelation { game_result: result, previous_boards: vec![previous_code], next_boards: next_boards.clone() });
 
-                println!("{:?}", result);
-                if result != GameResult::Unknown {
-                    return Option::Some(code);
-                }
-
-                for i in 0..B::get_board_size() {
-                    let child_code = self.search_recur(&board, i);
-                    if let Some(child_code) = child_code {
-                        next_boards.try_borrow_mut().unwrap().push(child_code);
+                    if result != GameResult::Unknown {
+                        return Option::Some(code);
                     }
                 }
             }
+
+            let mut codes = Vec::new();
+
+            for i in 0..B::get_board_size() {
+                let child_code = self.search_recur(&board, i);
+                if let Some(child_code) = child_code {
+                    codes.push(child_code);
+                }
+            }
+
+            let mut map = self.map.borrow_mut();
+            let relation = map.get_mut(&code).unwrap();
+            relation.next_boards.try_borrow_mut().unwrap().extend_from_slice(&codes);
 
             Option::Some(code)
         } else {
